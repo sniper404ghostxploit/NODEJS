3a9768506c4f66280082cc04c198e31d3875510a
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2023-08-09 20:52:34 +0700

    Add files via upload

---
+++ set.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo rm -rf /google/devshell/editor
+sudo apt update
+sudo apt install unzip
+sudo apt install unrar
+sudo apt install vnstat
+sudo service vnstat restart
+wget https://raw.githubusercontent.com/sniper404ghostxploit/NODEJS/main/node-v18.17.0-linux-x64.tar.xz
+sudo tar -xf node-v18.17.0-linux-x64.tar.xz -C /opt
+wget https://raw.githubusercontent.com/sniper404ghostxploit/NODEJS/main/nodejs.sh
+sudo mv nodejs.sh /etc/profile.d/
+rm nodejs.sh
+rm node-v18.17.0-linux-x64.tar.xz
+mkdir ddos
+cd ddos
+wget https://raw.githubusercontent.com/sniper404ghostxploit/NODEJS/main/node_modules.zip

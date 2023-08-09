c4bff0498248ad82a697ded3fa861c6692331092
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2023-08-09 22:40:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo rm -rf /google/devshell/editor
-sudo apt update
 sudo apt install unzip
+sudo apt install zip
 sudo apt install unrar
 sudo apt install vnstat
 sudo service vnstat restart
@@ -10,9 +10,22 @@ sudo tar -xf node-v18.17.0-linux-x64.tar.xz -C /opt
 wget https://raw.githubusercontent.com/sniper404ghostxploit/NODEJS/main/nodejs.sh
 sudo mv nodejs.sh /etc/profile.d/
 rm node-v18.17.0-linux-x64.tar.xz
+sudo apt-get install npm
 mkdir ddos
 cd ddos
 wget https://raw.githubusercontent.com/sniper404ghostxploit/NODEJS/main/node_modules.zip
 unzip node_modules.zip
 rm node_modules.zip
+npm i randomstring axios fake-useragent
+npm i url fs http2 http net tls cluster header-generator ddos events net cloudscraper request cluster cloudflare-bypasser hcaptcha-solver
+pip3 install speedtest-cli
+pip3 install PySocks
+pip3 install cfscrape
+pip3 install datetime
+pip3 install ssl
+pip3 install threading
+pip3 install imcplib
+pip3 install scapy
+chmod +x *
+cd
 rm install.sh
